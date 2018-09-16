6e920e0016bfb06b5b03170cc6813029aed8807c
Geeth sandaru Madhushan <geeth.gsm@gmail.com>
2018-09-16 22:29:10 +0530

    sendSms

--- ResoflexClientFeedBackSystem/Form1.Designer.cs
+++ ResoflexClientFeedBackSystem/Form1.Designer.cs
@@ -29,12 +29,94 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.msgTxtBox = new System.Windows.Forms.TextBox();
+            this.sendBtn = new System.Windows.Forms.Button();
+            this.notifyIconMailSuccessful = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBoxTV = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // msgTxtBox
+            // 
+            this.msgTxtBox.Location = new System.Drawing.Point(73, 42);
+            this.msgTxtBox.Multiline = true;
+            this.msgTxtBox.Name = "msgTxtBox";
+            this.msgTxtBox.Size = new System.Drawing.Size(372, 67);
+            this.msgTxtBox.TabIndex = 0;
+            // 
+            // sendBtn
+            // 
+            this.sendBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sendBtn.Location = new System.Drawing.Point(73, 180);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(126, 33);
+            this.sendBtn.TabIndex = 1;
+            this.sendBtn.Text = "Send";
+            this.sendBtn.UseVisualStyleBackColor = true;
+            this.sendBtn.Click += new System.EventHandler(this.sendBtn_Click);
+            // 
+            // notifyIconMailSuccessful
+            // 
+            this.notifyIconMailSuccessful.Text = "Email Sent Successfully";
+            this.notifyIconMailSuccessful.Visible = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Issue";
+            // 
+            // checkBoxTV
+            // 
+            this.checkBoxTV.AutoSize = true;
+            this.checkBoxTV.Location = new System.Drawing.Point(73, 136);
+            this.checkBoxTV.Name = "checkBoxTV";
+            this.checkBoxTV.Size = new System.Drawing.Size(108, 21);
+            this.checkBoxTV.TabIndex = 3;
+            this.checkBoxTV.Text = "TeamViewer";
+            this.checkBoxTV.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label2.Location = new System.Drawing.Point(197, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "RFS is Online!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBoxTV);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sendBtn);
+            this.Controls.Add(this.msgTxtBox);
+            this.Name = "Form1";
+            this.Text = "Bug Reporter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox msgTxtBox;
+        private System.Windows.Forms.Button sendBtn;
+        private System.Windows.Forms.NotifyIcon notifyIconMailSuccessful;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBoxTV;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- ResoflexClientFeedBackSystem/Form1.cs
+++ ResoflexClientFeedBackSystem/Form1.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +19,128 @@ namespace ResoflexClientFeedBackSystem
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void sendBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var fromAddress = new MailAddress("techninjas3.0v@gmail.com");
+                var fromPassword = "Techninjas@SLIIT";
+                var toAddress = new MailAddress("geeth.gsm@gmail.com");
+
+                string subject = "Bug Reporter";
+                string body = msgTxtBox.Text;
+
+                System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+                };
+
+                using (var message = new MailMessage(fromAddress, toAddress)
+                {
+                    Subject = subject,
+                    Body = body
+                })
+
+                    smtp.Send(message);
+
+                notifyIconMailSuccessful.Icon = SystemIcons.Application;
+                notifyIconMailSuccessful.BalloonTipText = "Email sent successfully!";
+                notifyIconMailSuccessful.ShowBalloonTip(200);
+
+                msgTxtBox.Text = "";
+
+                string result = sendSms("message");
+
+                if (result != null)
+                    msgTxtBox.Text = result;
+
+                notifyIconMailSuccessful.Icon = SystemIcons.Application;
+                notifyIconMailSuccessful.BalloonTipText = "SMS sent successfully!";
+                notifyIconMailSuccessful.ShowBalloonTip(200);
+
+                if (checkBoxTV.Checked)
+                {
+                    System.Diagnostics.Process process = new System.Diagnostics.Process();
+
+                    process.StartInfo.FileName = "C:\\Program Files (x86)\\TeamViewer\\TeamViewer.exe";
+                    process.EnableRaisingEvents = true;
+
+                    process.Start();
+
+                    process.WaitForExit();
+
+                    msgTxtBox.Text = "TeamViewer Exited";
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Something went wrong!\n" + exc, "Mail sender", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static bool PingHost(string nameOrAddress)
+        {
+            bool pingable = false;
+            Ping pinger = null;
+
+            try
+            {
+                pinger = new Ping();
+                PingReply reply = pinger.Send(nameOrAddress);
+                pingable = reply.Status == IPStatus.Success;
+            }
+            catch (PingException)
+            {
+                // Discard PingExceptions and return false;
+            }
+            finally
+            {
+                if (pinger != null)
+                {
+                    pinger.Dispose();
+                }
+            }
+
+            return pingable;
+        }
+
+        public string sendSms(string msg)
+        {
+            //String message = HttpUtility.UrlEncode("This is your message");
+            String message = "This is your message";
+            using (var wb = new WebClient())
+            {
+                byte[] response = wb.UploadValues("https://api.txtlocal.com/send/", new NameValueCollection()
+                {
+                {"apikey" , "vuHeQkjT1nE-kGtSpqkwMKOhPRGl2AGXtCDYvi4JGP"},
+                {"numbers" , "94777188617"},
+                {"message" , message},
+                {"sender" , "Jims Autos"}
+                });
+                string result = System.Text.Encoding.UTF8.GetString(response);
+                return result;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (PingHost("www.microsoft.com"))
+            {
+                checkBoxTV.Visible = true;
+                label2.Text = "RFS is Online!";
+            }
+            else
+            {
+                checkBoxTV.Visible = false;
+                label2.Text = "RFS is Offline!";
+            }
         }
     }
 }
